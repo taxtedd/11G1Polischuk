a572ff21d978b7a6e4bcd68b8dc573dfd03ab67f
taxtedd <89980575+taxtedd@users.noreply.github.com>
2021-11-22 22:03:39 +0300

    Update Запросы.sql

--- Запросы.sql
+++ Запросы.sql
@@ -7,8 +7,6 @@ SELECT SUBSTR(birth_date, 1, 4)  AS 'Birth date' FROM users GROUP BY SUBSTR(birt
 
 SELECT  COUNT(*)  AS 'Total items' FROM products;
 
-SELECT ((SUBSTR(CURRENT_DATE,1,4)-SUBSTR(birth_date,1,4))-(SUBSTR(CURRENT_DATE,6,5)<SUBSTR(birth_date,6,5))) AS 'Age' FROM users;
-
 SELECT AVG((SUBSTR(CURRENT_DATE,1,4)-SUBSTR(birth_date,1,4))-(SUBSTR(CURRENT_DATE,6,5)<SUBSTR(birth_date,6,5))) AS 'Age' FROM users
 WHERE SUBSTR(registration_date,9,2)<=SUBSTR(CURRENT_DATE,9,2) AND ((SUBSTR(CURRENT_DATE,1,4)-SUBSTR(registration_date,1,4))*12+(SUBSTR(CURRENT_DATE,6,2)-SUBSTR(registration_date,6,2)))<=2 OR
 SUBSTR(registration_date,9,2)>SUBSTR(CURRENT_DATE,9,2) AND ((SUBSTR(CURRENT_DATE,1,4)-SUBSTR(registration_date,1,4))*12+(SUBSTR(CURRENT_DATE,6,2)-SUBSTR(registration_date,6,2)))<=3;
