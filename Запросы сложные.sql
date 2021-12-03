73dcdee953dc2c616ed238869eceaefc43d84f48
taxtedd <89980575+taxtedd@users.noreply.github.com>
2021-12-04 00:21:44 +0300

    Update Запросы сложные.sql

--- Запросы сложные.sql
+++ Запросы сложные.sql
@@ -5,7 +5,7 @@ SELECT email, surname FROM users JOIN orders on users.id==orders.user_id;
 SELECT products.name AS 'Наименование',(SELECT warehouses.name FROM warehouses WHERE warehouses.id=warehousing.warehouse) AS 'Склад', warehousing.quantity AS 'Количество' FROM products LEFT JOIN warehousing
 WHERE warehousing.product=products.id
 
---Вывести товары каких категорий покупал каждый польователь, только если он хотя бы раз делал заказ
+--Вывести категории товаров, которые покупали польователи (только если они хотя бы раз делали заказ)
 SELECT users.email AS 'e-mail', (SELECT types.name FROM types WHERE types.id=
 	(SELECT products.type FROM products WHERE products.id = orders.id)) AS 'Тип товара' 
 FROM users JOIN  orders ON orders.user_id=users.id;
