36942b32cee85e30765a4d1ff26c977e65aa99f0
taxtedd <89980575+taxtedd@users.noreply.github.com>
2021-12-04 00:14:02 +0300

    Update

--- Запросы сложные.sql
+++ Запросы сложные.sql
@@ -5,7 +5,7 @@ SELECT email, surname FROM users JOIN orders on users.id==orders.user_id;
 SELECT products.name AS 'Наименование',(SELECT warehouses.name FROM warehouses WHERE warehouses.id=warehousing.warehouse) AS 'Склад', warehousing.quantity AS 'Количество' FROM products LEFT JOIN warehousing
 WHERE warehousing.product=products.id
 
---не доделала: Вывести пользователей, которым (меньше 18 лет) или (старше 60)
-SELECT users.email FROM users WHERE (SUBSTR(CURRENT_DATE,1,4)-SUBSTR(birth_date,1,4))-(SUBSTR(CURRENT_DATE,6,5)<SUBSTR(birth_date,6,5))<18
-UNION
-SELECT users.email FROM users, orders WHERE (SUBSTR(CURRENT_DATE,1,4)-SUBSTR(birth_date,1,4))-(SUBSTR(CURRENT_DATE,6,5)<SUBSTR(birth_date,6,5))>60 AND orders.user_id=users.id
+--Вывести товары каких категорий покупал каждый польователь, только если он хотя бы раз делал заказ
+SELECT users.email AS 'e-mail', (SELECT types.name FROM types WHERE types.id=
+	(SELECT products.type FROM products WHERE products.id = orders.id)) AS 'Тип товара' 
+FROM users JOIN  orders ON orders.user_id=users.id;
